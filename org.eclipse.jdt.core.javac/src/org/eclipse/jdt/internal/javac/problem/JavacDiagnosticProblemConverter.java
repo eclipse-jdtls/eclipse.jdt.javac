e41f486ef077c65d47741d06b46d7e2d0137b2b7
David Thompson <davthomp@redhat.com>
2026-03-24 13:48:07 -0400

    Convert "unreachable catch" problem
    eg.
    
    ```java
    public class UnreachableCatch {
    
    	static class ExceptionA extends Exception {
    
    	}
    	static class ExceptionB extends Exception {
    	}
    
    	public static void foo() {
    		try {
    			if (!true) {
    				throw new ExceptionB();
    			} else {
    				throw new ExceptionA();
    			}
    		} catch (ExceptionA e1) {
    
    		} catch (ExceptionB | ExceptionA e2) {
    
    		}
    	}
    
    }
    ```
    
    Signed-off-by: David Thompson <davthomp@redhat.com>

--- org.eclipse.jdt.core.javac/src/org/eclipse/jdt/internal/javac/problem/JavacDiagnosticProblemConverter.java
+++ org.eclipse.jdt.core.javac/src/org/eclipse/jdt/internal/javac/problem/JavacDiagnosticProblemConverter.java
@@ -1494,6 +1494,7 @@ public class JavacDiagnosticProblemConverter {
 			case "compiler.warn.auxiliary.class.accessed.from.outside.of.its.source.file" -> JavacProblemIds.AuxiliaryClassAccessedOutsideItsSourceFile;
 			case "compiler.warn.restricted.method" -> IProblem.DiscouragedReference;
 			case "compiler.note.deprecated.filename.additional" -> IProblem.UsingDeprecatedMethod;
+			case "compiler.warn.unreachable.catch" -> IProblem.UnreachableCatch;
 			case "compiler.note.deprecated.recompile" -> -1;
 			default -> {
 				ILog.get().error("Could not accurately convert diagnostic (" + diagnostic.getCode() + ")\n" + diagnostic);
